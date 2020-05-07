136306aa703231df2da7cccfa9e0641c17caa8c7
Keith Brink <keith.brink@gmail.com>
2020-05-07 17:56:28 +0300

    Add newer classes from CPIGroup

--- src/AmazonMerchantCore.php
+++ src/AmazonMerchantCore.php
@@ -20,17 +20,17 @@ namespace KeithBrink\AmazonMws;
  */
 
 /**
- * Core class for Amazon Finances API.
+ * Core class for Amazon Merchant Fulfillment API.
  *
- * This is the core class for all objects in the Amazon Finance section.
+ * This is the core class for all objects in the Amazon Merchant Fulfillment section.
  * It contains no methods in itself other than the constructor.
  */
-abstract class AmazonFinanceCore extends AmazonCore
+abstract class AmazonMerchantCore extends AmazonCore
 {
     /**
-     * AmazonFinanceCore constructor sets up key information used in all Amazon Finance Core requests.
+     * AmazonMerchantCore constructor sets up key information used in all Amazon Merchant Core requests.
      *
-     * This constructor is called when initializing all objects in the Amazon Finance Core.
+     * This constructor is called when initializing all objects in the Amazon Merchant Core.
      * The parameters are passed by the child objects' constructors, which are
      * in turn passed to the AmazonCore constructor. See it for more information
      * on these parameters and common methods.
@@ -41,22 +41,22 @@ abstract class AmazonFinanceCore extends AmazonCore
      * @param array|string $m [optional] <p>The files (or file) to use in Mock Mode.</p>
      * @param string $config [optional] <p>An alternate config file to set. Used for testing.</p>
      */
-    public function __construct($s = null, $mock = false, $m = null)
+    public function __construct($s = null, $mock = false, $m = null, $config = null)
     {
-        parent::__construct($s, $mock, $m);
+        parent::__construct($s, $mock, $m, $config);
         include $this->env;
 
-        if (isset($AMAZON_VERSION_FINANCE)) {
-            $this->urlbranch = 'Finances/'.$AMAZON_VERSION_FINANCE;
-            $this->options['Version'] = $AMAZON_VERSION_FINANCE;
+        if (isset($AMAZON_VERSION_MERCHANT)) {
+            $this->urlbranch = 'MerchantFulfillment/'.$AMAZON_VERSION_MERCHANT;
+            $this->options['Version'] = $AMAZON_VERSION_MERCHANT;
         }
 
-        if (isset($THROTTLE_LIMIT_FINANCE)) {
-            $this->throttleLimit = $THROTTLE_LIMIT_FINANCE;
+        if (isset($THROTTLE_LIMIT_MERCHANT)) {
+            $this->throttleLimit = $THROTTLE_LIMIT_MERCHANT;
         }
-        if (isset($THROTTLE_TIME_FINANCE)) {
-            $this->throttleTime = $THROTTLE_TIME_FINANCE;
+        if (isset($THROTTLE_TIME_MERCHANT)) {
+            $this->throttleTime = $THROTTLE_TIME_MERCHANT;
         }
-        $this->throttleGroup = 'Finance';
+        $this->throttleGroup = 'MerchantFulfillment';
     }
 }
